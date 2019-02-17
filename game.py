c5656d2b493e81c38f2c57bc04f3cc47847592a8
Zhuo Liu <imzzzoe@gmail.com>
2019-02-18 10:54:19 +1100

    output 9-16

--- game.py
+++ game.py
@@ -838,7 +838,7 @@ def chooseTrueGoal(start, goals):
   #   disFromStart[goal] = dist
   # return max(disFromStart, key=disFromStart.get)
   # return random.choice(goals)
-  return (3,4)
+  return (4,3)
 
 
 
